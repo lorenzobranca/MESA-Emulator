286caa155ffa252bd54feb9fbaa14d8f900a9b17
lorenzobranca <lollobranca@gmail.com>
2025-01-08 10:44:12 +0100

    updated hyperparameters and plots

--- mesaemulator.py
+++ mesaemulator.py
@@ -19,13 +19,14 @@ def data_descaler(data, max_data, min_data):
     data = (max_data - min_data)*data + min_data
     return data
 
+N_data = 53
 
 parser = argparse.ArgumentParser(description="train or predict.")
 parser.add_argument("mode", choices=["train", "predict"], help="Exec mode: 'train' or 'predict'.")
 args = parser.parse_args()
 
 # Define data directory
-data_dir = '/export/scratch/lbranca/binaries_project/mesaemulator/unaligned_data'
+data_dir = '/home/lollo/binaries/mesaemulator/unaligned_data'
 
 # Load and preprocess data
 time = np.loadtxt(data_dir + '/time_grid_interp_unaligned.dat')
@@ -59,11 +60,11 @@ data = dde.data.Triple(
 # DeepONet configuration
 m = 2
 dim_x = 1
-regularization = ['l2', 0.001]
+regularization = ['l2', 0.0001]
 activation = f"LAAF-{10} tanh"
 
 net = dde.nn.DeepONetCartesianProd(
-    [m, 32, 32, 32, 16],
+    [m, 16, 16, 16, 16],
     [dim_x, 32, 32, 32,  32],
     activation=activation,  # Adaptive activation
     kernel_initializer="Glorot normal",
@@ -74,7 +75,7 @@ net = dde.nn.DeepONetCartesianProd(
 
 # Compile 
 model = dde.Model(data, net)
-model.compile("adam", lr=0.0008, metrics=["mean l2 relative error"])
+model.compile("adam", lr=0.001, metrics=["mean l2 relative error"])
 
 
 
@@ -93,28 +94,65 @@ if args.mode == "train":
 
 elif args.mode == "predict":
 
-    model.restore('model_mesa/model-30018.ckpt',verbose=1)
+    eps = 0.1 # tolerance in log
+
+    model.restore('model_mesa/model-46109.ckpt', verbose=1)
 
     y_pred = model.predict(X_train)
-    
-    colormap = plt.cm.viridis
+
+    colormap = plt.cm.jet
     colors = [colormap(i) for i in np.linspace(0, 1, 53)]
 
-    for i in range(53):
+    # Plot for mass
+    fig, axs = plt.subplots(2, 1, gridspec_kw={'height_ratios': [4, 1]}, figsize=(10, 8))
 
-        plt.plot(timesteps, data_descaler(y_pred[i,:,0], max_mass, min_mass), lw=2, color = colors[i])
-        plt.plot(timesteps, data_descaler(y_train[i,:,0], max_mass, min_mass), lw=3, color = colors[i], ls = ':')
-        #plt.xlim(0.8)
+    for i in range(N_data):
+        # Main plot: predictions vs true values for mass
+        axs[0].plot(timesteps, data_descaler(y_pred[i,:,0], max_mass, min_mass), lw=2, color=colors[i])
+        axs[0].plot(timesteps, data_descaler(y_train[i,:,0], max_mass, min_mass), lw=3, color=colors[i], ls=':')
 
+        # Subplot: relative differences
+        relative_diff = (data_descaler(y_pred[i,:,0], max_mass, min_mass) - data_descaler(y_train[i,:,0], max_mass, min_mass)) \
+                        / (data_descaler(y_train[i,:,0], max_mass, min_mass) + eps) 
+        axs[1].plot(timesteps, relative_diff, lw=2, color=colors[i])
+
+    # Finalize mass plot
+    axs[0].set_ylabel('Log(Msun)')
+    axs[0].set_xlabel('Evolution stage')
+    axs[0].set_title('Mass Prediction vs True')
+
+    axs[1].set_ylabel('Rel. Diff.')
+    axs[1].set_xlabel('Evolution stage')
+    axs[1].axhline(0, color='black', lw=1, ls='--')
+
+    plt.tight_layout()
     plt.savefig('./plots/pred_vs_true_mass.png')
-
     plt.close()
 
-    for i in range(53):
+    # Plot for radius
+    fig, axs = plt.subplots(2, 1, gridspec_kw={'height_ratios': [4, 1]}, figsize=(10, 8))
 
-        plt.plot(timesteps, data_descaler(y_pred[i,:,1], max_radius, min_radius), lw=2, color = colors[i])
-        plt.plot(timesteps, data_descaler(y_train[i,:,1], max_radius, min_radius), lw=3, color = colors[i], ls = ':')
-        #plt.xlim(0.8)
+    for i in range(N_data):
+        # Main plot: predictions vs true values for radius
+        axs[0].plot(timesteps, data_descaler(y_pred[i,:,1], max_radius, min_radius), lw=2, color=colors[i])
+        axs[0].plot(timesteps, data_descaler(y_train[i,:,1], max_radius, min_radius), lw=3, color=colors[i], ls=':')
 
+        # Subplot: relative differences
+        relative_diff = (data_descaler(y_pred[i,:,1], max_radius, min_radius) - data_descaler(y_train[i,:,1], max_radius, min_radius)) \
+                        / (data_descaler(y_train[i,:,1], max_radius, min_radius) + eps)
+        axs[1].plot(timesteps, relative_diff, lw=2, color=colors[i])
+
+    # Finalize radius plot
+    axs[0].set_ylabel('Log(Rsun)')
+    axs[0].set_xlabel('Evolution stage')
+    axs[0].set_title('Radius Prediction vs True')
+
+    axs[1].set_ylabel('Rel. Diff.')
+    axs[1].set_xlabel('Evolution stage')
+    axs[1].axhline(0, color='black', lw=1, ls='--')
+
+    plt.tight_layout()
     plt.savefig('./plots/pred_vs_true_radius.png')
+    plt.close()
+
 
